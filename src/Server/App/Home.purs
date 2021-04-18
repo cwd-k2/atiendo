58f093e9984342bd6ec16772ddf428bbe44ef16a
cwd-k2 <kaito.augmented7th@gmail.com>
2021-04-18 09:47:39 +0900

    Server/App.purs に移動してみた

--- src/Server/App/Home.purs
+++ src/Server/App/Home.purs
@@ -1,9 +1,11 @@
 module Server.App.Home
-  ( home
+  ( homeResource
   , Home
   ) where
 
 import Prelude
+
+import Control.Monad.Except (ExceptT)
 
 import Text.Smolder.HTML (head, html, script, link, title)
 import Text.Smolder.HTML.Attributes (lang, src, href, rel)
@@ -11,10 +13,12 @@ import Text.Smolder.Markup (empty, text, (!))
 
 import Type.Trout.ContentType.HTML (class EncodeHTML)
 
+import Hyper.Trout.Router (RoutingError)
+
 data Home = Home
 
-home :: forall m. Applicative m => { "GET" :: m Home }
-home = { "GET": pure Home }
+homeResource :: forall m. Monad m => { "GET" :: ExceptT RoutingError m Home }
+homeResource = { "GET": pure Home }
 
 instance encodeHTMLHome :: EncodeHTML Home where
   encodeHTML Home =
@@ -22,4 +26,4 @@ instance encodeHTMLHome :: EncodeHTML Home where
       head $ do
         script ! src "/scripts/app.js" $ empty
         link ! href "/styles/app.css" ! rel "stylesheet"
-        title $ text "Atiend | Home"
+        title $ text "Atiendo | Home"
--- src/Server/Main.purs
+++ src/Server/Main.purs
@@ -4,47 +4,10 @@ import Prelude
 
 import Effect (Effect)
 
-import Control.Monad.Indexed.Qualified as Ix
-
-import Data.Maybe (maybe)
-import Data.MediaType.Common (textHTML)
-
 import Hyper.Node.Server (defaultOptionsWithLogging, runServer)
-import Hyper.Node.FileServer (fileServer)
-import Hyper.Status (statusNotFound)
-import Hyper.Middleware (Middleware)
-import Hyper.Conn (Conn)
-import Hyper.Response ( closeHeaders, contentType, respond, writeStatus
-                      , StatusLineOpen, ResponseEnded
-                      , class Response, class ResponseWritable
-                      )
 
-import Hyper.Trout.Router (router)
-
-import Server.App (app)
-import Server.App.Home (home)
-
-notFound :: forall m req res c b
-  .  Monad m
-  => Response res m b
-  => ResponseWritable b m String
-  => Middleware m (Conn req (res StatusLineOpen) c) (Conn req (res ResponseEnded) c) Unit
-notFound = Ix.do
-  writeStatus statusNotFound
-  contentType textHTML
-  closeHeaders
-  respond "Not Found"
+import Server.App (appRouter)
 
 main :: Effect Unit
 main = do
   runServer defaultOptionsWithLogging {} appRouter
-  where
-    onRoutingError status msg
-      | status == statusNotFound = fileServer "public" notFound
-      | otherwise = Ix.do
-        writeStatus status
-        contentType textHTML
-        closeHeaders
-        respond (maybe "" identity msg)
-
-    appRouter = router app home onRoutingError
